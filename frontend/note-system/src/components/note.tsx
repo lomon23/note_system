4906b8593fecc5c99edf6f9496605cb666c5a1b9
lomon23 <lomontav22@gmail.com>
2025-09-14 14:42:10 +0000

    write handle click fow new tabs

--- frontend/note-system/src/components/note.tsx
+++ frontend/note-system/src/components/note.tsx
@@ -1,14 +1,43 @@
-import react from 'react';
+import React, { useState, useEffect } from "react";
 import './style_note.css';
 
 
 const Note: React.FC = () => {
+    const [notes, setNotes] = useState<number[]>([1]);
+
+    useEffect (() => {
+        const handleKeyDown = (event:KeyboardEvent) => {
+            if (event.key === "Enter" && event.altKey) {
+                setNotes((prev) => [...prev, prev.length + 1]);
+            }
+        };
+
+
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+
+    }, []);
+    const handleDelete = (noteId: number, event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === "Delete") {
+            setNotes((prev) => prev.filter((n) =>  n !== noteId));
+        }
+    };
     return (
         <div className = "container_for_note">
-            <div className = "note_area">
-                <textarea className = "text_area" placeholder = "Напиши свою нотатку..." />
-            </div>
+            {notes.map((note) => (
+                <div key = {note} className = "note_area">
+                    <textarea 
+                        className = "text_area" 
+                        placeholder = {`Напиши свою нотатку...`} 
+                        onKeyDown = {(e) => handleDelete(note, e)}
+                    />
+                </div>
+            ))}
         </div>
     );
 };
+
 export default Note;
